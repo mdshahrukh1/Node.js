0e81a24adc63e6403314dbfb97d32ca9f48124c6
Md Shahrukh <shahrukh80912@gmail.com>
2025-02-17 11:32:53 +0530

    Session-9

---
+++ Session_9/connect.js
@@ -0,0 +1,32 @@
+const { MongoClient } = require('mongodb');
+const uri = 'mongodb://localhost:27017/';
+
+// MongoClient.connect(uri)
+// .then(resp=>console.log('Connected'))
+// .catch(err=>console.log('Error',err))
+
+async function main() {
+    try {
+        const client = await MongoClient.connect(uri);
+        console.log('Connected');
+        const db = client.db('skillacademy');
+        const collection = db.collection('student');
+
+        const insertResult = await collection.insertOne
+
+        ({name:'krizel',email:'krizzu@gmail.com',country:'India'});
+        console.log('Inserted documents =>', insertResult);
+
+    } catch (error) {
+        console.log('Error', error);
+    }
+}
+main();
+
+// Default PORTS
+// ----------------
+// mySQL 3306
+// Mongodb 27017
+// HTTP 8080
+// FTP 21
+// SSH 22
